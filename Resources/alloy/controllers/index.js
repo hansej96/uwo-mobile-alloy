7282aa57e76219bd816db686cff4c440de229ec5
Shawn Hansen <shawn.hansen@gmail.com>
2013-02-15 07:58:32 -0600

    Getting icons laid out on opening screen.

--- Resources/alloy/controllers/index.js
+++ Resources/alloy/controllers/index.js
@@ -1,28 +1,55 @@
 function Controller() {
-    function doClick(e) {
-        alert($.label.text);
-    }
     require("alloy/controllers/BaseController").apply(this, Array.prototype.slice.call(arguments));
     $model = arguments[0] ? arguments[0].$model : null;
     var $ = this, exports = {}, __defers = {};
     $.__views.index = A$(Ti.UI.createWindow({
-        backgroundColor: "#fff",
+        backgroundImage: "/Default.png",
         id: "index"
     }), "Window", null);
     $.addTopLevelView($.__views.index);
-    $.__views.label = A$(Ti.UI.createLabel({
-        width: Ti.UI.SIZE,
-        height: Ti.UI.SIZE,
-        color: "#000",
-        text: "Hello, World",
-        id: "label"
-    }), "Label", $.__views.index);
-    $.__views.index.add($.__views.label);
-    doClick ? $.__views.label.on("click", doClick) : __defers["$.__views.label!click!doClick"] = !0;
+    $.__views.news = A$(Ti.UI.createView({
+        backgroundImage: "/images/icons/news.png",
+        height: 85,
+        width: 102,
+        top: 0,
+        left: "auto",
+        id: "news",
+        image: "/images/icons/news.png"
+    }), "View", $.__views.index);
+    $.__views.index.add($.__views.news);
+    $.__views.people = A$(Ti.UI.createView({
+        id: "people",
+        image: "/images/icons/people.png"
+    }), "View", $.__views.index);
+    $.__views.index.add($.__views.people);
+    $.__views.links = A$(Ti.UI.createView({
+        id: "links",
+        image: "/images/icons/links.png"
+    }), "View", $.__views.index);
+    $.__views.index.add($.__views.links);
+    $.__views.connected = A$(Ti.UI.createView({
+        id: "connected",
+        image: "/images/icons/connected.png"
+    }), "View", $.__views.index);
+    $.__views.index.add($.__views.connected);
+    $.__views.services = A$(Ti.UI.createView({
+        id: "services",
+        image: "/images/icons/services.png"
+    }), "View", $.__views.index);
+    $.__views.index.add($.__views.services);
+    $.__views.emergency = A$(Ti.UI.createView({
+        backgroundImage: "/images/icons/emergency.png",
+        height: 85,
+        width: 102,
+        top: 0,
+        left: "auto",
+        id: "emergency",
+        image: "/images/icons/emergency.png"
+    }), "View", $.__views.index);
+    $.__views.index.add($.__views.emergency);
     exports.destroy = function() {};
     _.extend($, $.__views);
     $.index.open();
-    __defers["$.__views.label!click!doClick"] && $.__views.label.on("click", doClick);
     _.extend($, exports);
 }
 
