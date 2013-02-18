f7293d2d309b26dc72f058559bdc8ab20541753e
Shawn Hansen <shawn.hansen@gmail.com>
2013-02-18 14:53:54 -0600

    Implemented NavigationGroup for iOS platform. Navbar is hidden on index and shown on all other pages. This is done by setting <Window>'s navBarHidden property in the corresponding .tss file.

--- app/controllers/index.js
+++ app/controllers/index.js
@@ -1,25 +1,53 @@
-function newsEventsClick(e) {
-    Alloy.createController('newsEvents').getView().open();
-}
+if (OS_IOS || OS_MOBILEWEB) {
+    Alloy.CFG.navgroup = $.navgroup;
+    
+    function newsEventsClick(e) {
+        $.navgroup.open(Alloy.createController('newsEvents').getView());
+    }
+    
+    function peoplePlacesClick(e) {
+        $.navgroup.open(Alloy.createController('peoplePlaces').getView());
+    }
+    
+    function quickLinksClick(e) {
+        $.navgroup.open(Alloy.createController('quickLinks').getView());
+    }
+    
+    function stayConnectedClick(e) {
+        $.navgroup.open(Alloy.createController('stayConnected').getView());
+    }
+    
+    function campusServicesClick(e) {
+        $.navgroup.open(Alloy.createController('campusServices').getView());
+    }
+    
+    function emergencyClick(e) {
+        $.navgroup.open(Alloy.createController('emergency').getView());
+    }
+} else {
+    function newsEventsClick(e) {
+        Alloy.createController('newsEvents').getView().open();
+    }
 
-function peoplePlacesClick(e) {
-    Alloy.createController('peoplePlaces').getView().open();
-}
+    function peoplePlacesClick(e) {
+        Alloy.createController('peoplePlaces').getView().open();
+    }
 
-function quickLinksClick(e) {
-    Alloy.createController('quickLinks').getView().open();
-}
+    function quickLinksClick(e) {
+        Alloy.createController('quickLinks').getView().open();
+    }
 
-function stayConnectedClick(e) {
-    Alloy.createController('stayConnected').getView().open();
-}
+    function stayConnectedClick(e) {
+        Alloy.createController('stayConnected').getView().open();
+    }
 
-function campusServicesClick(e) {
-    Alloy.createController('campusServices').getView().open();
-}
+    function campusServicesClick(e) {
+        Alloy.createController('campusServices').getView().open();
+    }
 
-function emergencyClick(e) {
-    Alloy.createController('emergency').getView().open();
+    function emergencyClick(e) {
+        Alloy.createController('emergency').getView().open();
+    }
 }
 
 $.index.open();
