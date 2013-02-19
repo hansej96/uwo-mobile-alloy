a5578410432c217603e3473ba4fa2171a1935770
Shawn Hansen <shawn.hansen@gmail.com>
2013-02-19 12:51:59 -0600

    Implemented Campus Map.
    Implemented Quick Links.

--- app/controllers/peoplePlaces.js
+++ app/controllers/peoplePlaces.js
@@ -2,8 +2,14 @@ if (OS_IOS || OS_MOBILEWEB) {
     function parkingMapClick(e) {
         Alloy.CFG.navgroup.open(Alloy.createController('parkingMap').getView());
     }
+    function campusMapClick(e) {
+        Alloy.CFG.navgroup.open(Alloy.createController('campusMap').getView());
+    }
 } else {
     function parkingMapClick(e) {
         Alloy.createController('parkingMap').getView().open();
+    }
+    function campusMapClick(e) {
+        Alloy.createController('campusMap').getView().open();
     }
 }
---
+++ app/controllers/quickLinks.js
@@ -0,0 +1,15 @@
+function titanWebClick(e) {
+    Ti.Platform.openURL("https://titanweb5.uwosh.edu/titanweb_prd/signon.html");
+}
+
+function d2lClick(e) {
+    Ti.Platform.openURL("https://uwosh.courses.wisconsin.edu/Shibboleth.sso/Login?target=https://uwosh.courses.wisconsin.edu/d2l/shibbolethSSO/deepLinkLogin.d2l");
+}
+
+function titanEmailClick(e) {
+    Ti.Platform.openURL("https://mail.google.com");
+}
+
+function mobileSetupClick(e) {
+    Ti.Platform.openURL("http://www.uwosh.edu/titanapps/mobile-devices");
+}
