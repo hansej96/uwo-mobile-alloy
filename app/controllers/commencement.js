a53b4ed4455496b255caaceea69549fcc9530f7d
Shawn Hansen <shawn.hansen@gmail.com>
2013-02-22 16:00:26 -0600

    Added Commencement Facebook, website and program logic.

--- app/controllers/commencement.js
+++ app/controllers/commencement.js
@@ -1,11 +1,35 @@
 if (OS_IOS || OS_MOBILEWEB) {
+    function commencementFacebookClick(e) {
+        Ti.Platform.openURL("http://m.facebook.com/UWOshkoshCommencement");
+    }
+    
     function commencementMapClick(e) {
         Alloy.CFG.navgroup.open(Alloy.createController('commencementMap').getView());
     }
     
+    function commencementWebsiteClick(e) {
+        Ti.Platform.openURL("http://www.uwosh.edu/commencement/");
+    }
+    
+    function commencementProgramClick(e) {
+        Ti.Platform.openURL("http://www.uwosh.edu/commencement/documents/program.pdf");
+    }
+    
 } else {
+    function commencementFacebookClick(e) {
+        Ti.Platform.openURL("http://m.facebook.com/UWOshkoshCommencement");
+    }
+    
     function commencementMapClick(e) {
         Alloy.createController('commencementMap').getView().open();
+    }
+    
+    function commencementWebsiteClick(e) {
+        Ti.Platform.openURL("http://www.uwosh.edu/commencement/");
+    }
+    
+    function commencementProgramClick(e) {
+        Ti.Platform.openURL("http://www.uwosh.edu/commencement/documents/program.pdf");
     }
     
 }
