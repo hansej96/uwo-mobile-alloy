d7bfca072c93fad6f32ae4bdbe5d03d4a90e807e
Shawn Hansen <shawn.hansen@gmail.com>
2013-02-26 15:05:15 -0600

    Added ability to view panoramas to locationSearchDetail.

--- app/controllers/locationSearchDetail.js
+++ app/controllers/locationSearchDetail.js
@@ -4,7 +4,11 @@ $.id.text = args.location.id;
 $.title.text = args.location.title;
 $.description.text = args.location.description;
 $.address.text = args.location.address;
-$.image.text = args.location.image;
+$.image.src = args.location.image;
 $.panorama.text = args.location.panorama;
 $.latitude.text = args.location.latitude;
 $.longitude.text = args.location.longitude;
+
+function panoramaClick (e) {
+    Ti.Platform.openURL(args.location.panorama);
+}
