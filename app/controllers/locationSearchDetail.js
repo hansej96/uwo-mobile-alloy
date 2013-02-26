936852e90341610ce00971788df15dd816c58fd8
Shawn Hansen <shawn.hansen@gmail.com>
2013-02-26 16:37:36 -0600

    Trying to get ImageView working for locationSearchDetail. No luck so far. Have put a click handler on the image row to verify that the image exists at that url, it does.

--- app/controllers/locationSearchDetail.js
+++ app/controllers/locationSearchDetail.js
@@ -4,11 +4,15 @@ $.id.text = args.location.id;
 $.title.text = args.location.title;
 $.description.text = args.location.description;
 $.address.text = args.location.address;
-$.image.src = args.location.image;
+$.image.image = args.location.image;
 $.panorama.text = args.location.panorama;
 $.latitude.text = args.location.latitude;
 $.longitude.text = args.location.longitude;
 
 function panoramaClick (e) {
     Ti.Platform.openURL(args.location.panorama);
+}
+
+function imageClick (e) {
+    Ti.Platform.openURL(args.location.image);
 }
