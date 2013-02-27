922d7cab906fc91916af61b385524c09d1402942
Shawn Hansen <shawn.hansen@gmail.com>
2013-02-27 14:43:24 -0600

    Added logic for Facebook, youtube, and foursquare to stayConnected.

--- app/controllers/stayConnected.js
+++ app/controllers/stayConnected.js
@@ -3,9 +3,33 @@ if (OS_IOS || OS_MOBILEWEB) {
         Alloy.CFG.navgroup.open(Alloy.createController('twitterAccountList').getView());
     }
 
+    function facebookAccountClick(e) {
+        Alloy.CFG.navgroup.open(Alloy.createController('webViews', { url: "https://m.facebook.com/uwoshkosh", title: "UWO Facebook"}).getView());
+    }
+
+    function youtubeAccountClick(e) {
+        Alloy.CFG.navgroup.open(Alloy.createController('webViews', { url: "http://www.youtube.com/uwosh", title: "UWO YouTube"}).getView());
+    }
+
+    function foursquareClick(e) {
+        Alloy.CFG.navgroup.open(Alloy.createController('webViews', { url: "/html/foursquare.html", title: "Foursquare"}).getView());
+    }
+
 } else {
     function twitterAccountClick(e) {
         Alloy.createController('twitterAccountList').getView().open();
+    }
+
+    function facebookAccountClick(e) {
+        Alloy.createController('webViews', { url: "https://m.facebook.com/uwoshkosh", title: "UWO Facebook"}).getView().open();
+    }
+
+    function youtubeAccountClick(e) {
+        Alloy.createController('webViews', { url: "https://http://www.youtube.com/uwosh", title: "UWO YouTube"}).getView().open();
+    }
+
+    function foursquareClick(e) {
+        Alloy.createController('webViews', { url: "/html/foursquare.html", title: "Foursquare"}).getView().open();
     }
 
 }
