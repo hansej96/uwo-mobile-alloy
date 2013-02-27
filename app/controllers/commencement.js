7d76a3590924132380ed8d2f04d1a26040c49d64
Shawn Hansen <shawn.hansen@gmail.com>
2013-02-27 15:27:21 -0600

    Changed commencement to use webViews. Also added @uwocommencement logic.

--- app/controllers/commencement.js
+++ app/controllers/commencement.js
@@ -1,35 +1,43 @@
 if (OS_IOS || OS_MOBILEWEB) {
     function commencementFacebookClick(e) {
-        Ti.Platform.openURL("http://m.facebook.com/UWOshkoshCommencement");
+        Alloy.CFG.navgroup.open(Alloy.createController('webViews' { url: "http://m.facebook.com/UWOshkoshCommencement", title: "UWO Commencement"}).getView());
     }
-    
+
+    function commencementTwitterClick(e) {
+        Alloy.CFG.navgroup.open(Alloy.createController('webViews' { url: "https://twitter.com/uwocommencement", title: "@uwocommencement"}).getView());
+    }
+
     function commencementMapClick(e) {
         Alloy.CFG.navgroup.open(Alloy.createController('commencementMap').getView());
     }
-    
+
     function commencementWebsiteClick(e) {
-        Ti.Platform.openURL("http://www.uwosh.edu/commencement/");
+        Alloy.CFG.navgroup.open(Alloy.createController('webViews' { url: "http://www.uwosh.edu/commencement/", title: "UWO Commencement"}).getView());
     }
-    
+
     function commencementProgramClick(e) {
-        Ti.Platform.openURL("http://www.uwosh.edu/commencement/documents/program.pdf");
+        Alloy.CFG.navgroup.open(Alloy.createController('webViews' { url: "http://www.uwosh.edu/commencement/documents/program.pdf", title: "Commencement Program"}).getView());
     }
-    
+
 } else {
     function commencementFacebookClick(e) {
-        Ti.Platform.openURL("http://m.facebook.com/UWOshkoshCommencement");
+        Alloy.createController('webViews' { url: "http://m.facebook.com/UWOshkoshCommencement", title: "UWO Commencement"}).getView().open();
     }
-    
+
+    function commencementTwitterClick(e) {
+        Alloy.createController('webViews' { url: "https://twitter.com/uwocommencement", title: "@uwocommencement"}).getView().open();
+    }
+
     function commencementMapClick(e) {
         Alloy.createController('commencementMap').getView().open();
     }
-    
+
     function commencementWebsiteClick(e) {
-        Ti.Platform.openURL("http://www.uwosh.edu/commencement/");
+        Alloy.createController('webViews' { url: "http://www.uwosh.edu/commencement/", title: "UWO Commencement"}).getView().open();
     }
-    
+
     function commencementProgramClick(e) {
-        Ti.Platform.openURL("http://www.uwosh.edu/commencement/documents/program.pdf");
+        Alloy.createController('webViews' { url: "http://www.uwosh.edu/commencement/documents/program.pdf", title: "Commencement Program"}).getView().open();
     }
-    
+
 }
