d3fb8b1ce3493ac85e05dcd8c25360e1721632d7
Shawn Hansen <shawn.hansen@gmail.com>
2013-02-27 15:30:22 -0600

    Oops, forgot a comma.

--- app/controllers/commencement.js
+++ app/controllers/commencement.js
@@ -1,10 +1,10 @@
 if (OS_IOS || OS_MOBILEWEB) {
     function commencementFacebookClick(e) {
-        Alloy.CFG.navgroup.open(Alloy.createController('webViews' { url: "http://m.facebook.com/UWOshkoshCommencement", title: "UWO Commencement"}).getView());
+        Alloy.CFG.navgroup.open(Alloy.createController('webViews', { url: "http://m.facebook.com/UWOshkoshCommencement", title: "UWO Commencement"}).getView());
     }
 
     function commencementTwitterClick(e) {
-        Alloy.CFG.navgroup.open(Alloy.createController('webViews' { url: "https://twitter.com/uwocommencement", title: "@uwocommencement"}).getView());
+        Alloy.CFG.navgroup.open(Alloy.createController('webViews', { url: "https://twitter.com/uwocommencement", title: "@uwocommencement"}).getView());
     }
 
     function commencementMapClick(e) {
@@ -12,20 +12,20 @@ if (OS_IOS || OS_MOBILEWEB) {
     }
 
     function commencementWebsiteClick(e) {
-        Alloy.CFG.navgroup.open(Alloy.createController('webViews' { url: "http://www.uwosh.edu/commencement/", title: "UWO Commencement"}).getView());
+        Alloy.CFG.navgroup.open(Alloy.createController('webViews', { url: "http://www.uwosh.edu/commencement/", title: "UWO Commencement"}).getView());
     }
 
     function commencementProgramClick(e) {
-        Alloy.CFG.navgroup.open(Alloy.createController('webViews' { url: "http://www.uwosh.edu/commencement/documents/program.pdf", title: "Commencement Program"}).getView());
+        Alloy.CFG.navgroup.open(Alloy.createController('webViews', { url: "http://www.uwosh.edu/commencement/documents/program.pdf", title: "Commencement Program"}).getView());
     }
 
 } else {
     function commencementFacebookClick(e) {
-        Alloy.createController('webViews' { url: "http://m.facebook.com/UWOshkoshCommencement", title: "UWO Commencement"}).getView().open();
+        Alloy.createController('webViews', { url: "http://m.facebook.com/UWOshkoshCommencement", title: "UWO Commencement"}).getView().open();
     }
 
     function commencementTwitterClick(e) {
-        Alloy.createController('webViews' { url: "https://twitter.com/uwocommencement", title: "@uwocommencement"}).getView().open();
+        Alloy.createController('webViews', { url: "https://twitter.com/uwocommencement", title: "@uwocommencement"}).getView().open();
     }
 
     function commencementMapClick(e) {
@@ -33,11 +33,11 @@ if (OS_IOS || OS_MOBILEWEB) {
     }
 
     function commencementWebsiteClick(e) {
-        Alloy.createController('webViews' { url: "http://www.uwosh.edu/commencement/", title: "UWO Commencement"}).getView().open();
+        Alloy.createController('webViews', { url: "http://www.uwosh.edu/commencement/", title: "UWO Commencement"}).getView().open();
     }
 
     function commencementProgramClick(e) {
-        Alloy.createController('webViews' { url: "http://www.uwosh.edu/commencement/documents/program.pdf", title: "Commencement Program"}).getView().open();
+        Alloy.createController('webViews', { url: "http://www.uwosh.edu/commencement/documents/program.pdf", title: "Commencement Program"}).getView().open();
     }
 
 }
