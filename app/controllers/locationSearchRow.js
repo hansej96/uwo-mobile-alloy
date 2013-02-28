b524490f528de0093b60e458cf27af7ef64633b3
Shawn Hansen <shawn.hansen@gmail.com>
2013-02-28 14:25:00 -0600

    Updated locationSearch to use filterAttribute

--- app/controllers/locationSearchRow.js
+++ app/controllers/locationSearchRow.js
@@ -2,16 +2,18 @@ var args = arguments[0] || {};
 var row = [];
 row.location = args.item;
 
+
 $.title.text = row.location.title;
+$.row.filter = row.location.title;
 
 if (OS_IOS || OS_MOBILEWEB) {
     function openDetail(e) {
         Alloy.CFG.navgroup.open(Alloy.createController('locationSearchDetail', { location: row.location }).getView());
     }
-    
+
 } else {
     function openDetail(e) {
         Alloy.createController('locationSearchDetail', { location: row.location }).getView().open();
     }
-    
+
 }
