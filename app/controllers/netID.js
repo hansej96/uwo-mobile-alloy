a2dd840f3b6807ff8ce98a86f3966109069ca145
Shawn Hansen <shawn.hansen@gmail.com>
2013-03-01 10:48:58 -0600

    Auth logic working. Wiring up to titancard.js now.

--- app/controllers/netID.js
+++ app/controllers/netID.js
@@ -1,18 +1,25 @@
 var portal = require("myuwoWS");
 
+function authSuccess() {
+    Ti.App.Properties.setBool("myUwoAuth", true);
+    Ti.App.fireEvent('return.auth');
+}
+
 function loginClick (e) {
-    $.userName.blur();
+    $.username.blur();
     $.password.blur();
 
-    if (username.value !== '' && password.value !== '') {
+    if ($.username.value !== '' && $.password.value !== '') {
         var params = {
             username: $.username.value,
-            password: password.value,
+            password: $.password.value,
             success: authSuccess,
             onerror: function() {
                 alert("An error occured")
             }
         };
         portal.auth(params);
+    } else {
+        alert("Username/Password are required")
     }
 }
