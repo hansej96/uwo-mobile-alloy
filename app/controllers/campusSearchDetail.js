21d964128fb60a0535a82533058ed0bf74411b8d
Shawn Hansen <shawn.hansen@gmail.com>
2013-03-02 19:31:49 -0600

    Made campusSearch phone numbers click-callable.

--- app/controllers/campusSearchDetail.js
+++ app/controllers/campusSearchDetail.js
@@ -5,3 +5,7 @@ $.dept.text = args.person.dept;
 $.email.text = args.person.email;
 $.phone.text = "(920) 424-" + args.person.phone;
 $.office.text = args.person.office;
+
+function phoneClick(e) {
+    Ti.Platform.openURL("tel://920424" + args.person.phone);
+}
