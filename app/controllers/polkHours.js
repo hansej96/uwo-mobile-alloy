b0b41580c78a10f9b89e38ae319a92b32bc7abac
Shawn Hansen <shawn.hansen@gmail.com>
2013-03-05 09:25:03 -0600

    Polk Library hours completed.

--- app/controllers/polkHours.js
+++ app/controllers/polkHours.js
@@ -1,16 +1,34 @@
 var webservices = require("libraryWS"),
-    datetime = require("datetimeJS");
+    moment = require("alloy/moment");
 
 function hours(nowOpen, hours, entry) {
-    var data = [];
+    $.currently.text = "Currently: " + nowOpen;
+    $.todayHours.text = "Today's Hours: " + moment(hours[0].open).format("h:mm a") + " - " + moment(hours[0].close).format("h:mm a");
 
-    var todayHeader = Ti.UI.createTableViewRow();
-    var todayHeaderLabel = Titanium.UI.createLabel({
-        text: 'Today'
-    });
+    $.day1.text = hours[1].title;
+    $.day1Hours.text = moment(hours[1].open).format("h:mm a") + " - " + moment(hours[1].close).format("h:mm a");
+    $.day2.text = hours[2].title;
+    $.day2Hours.text = moment(hours[2].open).format("h:mm a") + " - " + moment(hours[2].close).format("h:mm a");
+    $.day3.text = hours[3].title;
+    $.day3Hours.text = moment(hours[3].open).format("h:mm a") + " - " + moment(hours[3].close).format("h:mm a");
+    $.day4.text = hours[4].title;
+    $.day4Hours.text = moment(hours[4].open).format("h:mm a") + " - " + moment(hours[4].close).format("h:mm a");
+}
 
-    var status = Ti.UI.createTableViewRow();
-    var statusTitleLabel = Titanium.UI.createLabel({
-        text: 'Currently:'
+function getLibrary(entry) {
+    webservices.hours({
+        success: hours,
+        onerror: onerror,
+        entry: entry
     });
+}
+
+function onerror() {
+    alert("There was an error retrieving the library hours");
+}
+
+if(Ti.Network.online) {
+    getLibrary("Hours");
+} else {
+    alert("No network connection detected");
 }
