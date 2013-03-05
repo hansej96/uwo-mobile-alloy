49c7f08b4ef3d3afd9c6f1fa764dd8f10e0c402a
Shawn Hansen <shawn.hansen@gmail.com>
2013-03-05 10:21:39 -0600

    Available Computers for Polk Library working.

--- app/controllers/polkLibrary.js
+++ app/controllers/polkLibrary.js
@@ -3,9 +3,25 @@ if (OS_IOS || OS_MOBILEWEB) {
         Alloy.CFG.navgroup.open(Alloy.createController('polkHours').getView());
     }
 
+    function availableComputersClick(e) {
+        Alloy.CFG.navgroup.open(Alloy.createController('polkComputers').getView());
+    }
+
+    function groupFinderClick(e) {
+        Alloy.CFG.navgroup.open(Alloy.createController('groupFinder').getView());
+    }
+
 } else {
     function polkHoursClick(e) {
         Alloy.createController('polkHours').getView().open();
+    }
+
+    function availableComputersClick(e) {
+        Alloy.createController('polkComputers').getView().open();
+    }
+
+    function groupFinderClick(e) {
+        Alloy.createController('groupFinder').getView().open();
     }
 
 }
