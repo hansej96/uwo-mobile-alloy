5df8415c5d86697dc77aad5284d848b78b2c999a
Shawn Hansen <shawn.hansen@gmail.com>
2013-03-06 15:49:57 -0600

    Orientation and device specific checks are in place. Android isn't cooperating on dashboard layout yet. Still need to check for icon sizes in sub-navigation.

--- app/controllers/commencement.js
+++ app/controllers/commencement.js
@@ -41,3 +41,112 @@ if (OS_IOS || OS_MOBILEWEB) {
     }
 
 }
+
+var imageSuffix = '',
+    screenHeight = Ti.Platform.displayCaps.platformHeight,
+    srceenWidth = Ti.Platform.displayCaps.platformWidth;
+
+if(Alloy.Globals.isLargeScreen()) {
+    $.fb.height = 170;
+    $.fb.width = 204;
+    $.tw.height = 170;
+    $.tw.width = 204;
+    $.maps.height = 170;
+    $.maps.width = 204;
+    $.website.height = 170;
+    $.website.width = 204;
+    $.program.height = 170;
+    $.program.width = 204;
+    $.check.height = 170;
+    $.check.width = 204;
+    imageSuffix = "_ipad";
+}
+
+if(Alloy.Globals.isAndroid() && Alloy.Globals.isHighDensityScreen()) {
+    imageSuffix = "@2x";
+}
+
+$.fb.image = "/images/commence/fb" + imageSuffix + ".png";
+$.tw.image = "/images/commence/tw" + imageSuffix + ".png";
+$.maps.image = "/images/commence/maps" + imageSuffix + ".png";
+$.website.image = "/images/commence/website" + imageSuffix + ".png";
+$.program.image = "/images/commence/program" + imageSuffix + ".png";
+$.check.image = "/images/commence/check" + imageSuffix + ".png";
+
+// Set proper dashboard properties
+if(Alloy.Globals.isLargeScreen()) {
+    options = {
+        mainBackgroundImage: '/images/grad_home_ipad.png',
+        dashboardHeight: 340,
+        dashboardWidth: 612,
+        mainBackgroundImageLandscape: '/images/grad_home_ipad_l.png',
+        dashboardHeightLandscape: 510,
+        dashboardWidthLandscape: 410,
+        dashboardBottom: 120,
+        dashboardBottomLandscape: 130
+    };
+} else if(Alloy.Globals.isHighDensityScreen() && Alloy.Globals.isAndroid()) {
+    options = {
+        dashboardHeight: 170,
+        dashboardWidth: 306,
+        dashboardHeightLandscape: 260,
+        dashboardWidthLandscape: 220,
+        mainBackgroundImage: '/images/grad_home@2x.png',
+        mainBackgroundImageLandscape: '/images/grad_home_l@2x.png',
+        dashboardBottom: 40,
+        dashboardBottomLandscape: 20
+    };
+    Ti.API.info(screenHeight);
+    // scale for android heights
+    if(screenHeight == '569.0') {
+        options.mainBackgroundImage = '/images/grad_home_854.png';
+        options.mainBackgroundImageLandscape = '/images/grad_home_854_l.png';
+        options.dashboardBottom = 80;
+    } else if(screenHeight == '533.0') {
+        options.mainBackgroundImage = '/images/grad_home_800.png';
+        options.mainBackgroundImageLandscape = '/images/grad_home_800_l.png';
+    }
+} else {
+    options = {
+        mainBackgroundImage: '/images/grad_home.png',
+        mainBackgroundImageLandscape: '/images/grad_home_l.png',
+        dashboardHeight: 170,
+        dashboardWidth: 306,
+        dashboardBottom: 40,
+        dashboardHeightLandscape: 260,
+        dashboardWidthLandscape: 220,
+        dashboardBottomLandscape: 20
+    };
+}
+
+// Set initial orientation
+if(Ti.Gesture.orientation == Titanium.UI.PORTRAIT || Ti.Gesture.orientation == Titanium.UI.UPSIDE_PORTRAIT) {
+    $.commencementWindow.backgroundImage = options.mainBackgroundImage;
+    $.commencementDashboard.height = options.dashboardHeight;
+    $.commencementDashboard.width = options.dashboardWidth;
+    $.commencementDashboard.bottom = options.dashboardBottom;
+    $.commencementDashboard.left = "auto";
+}
+if(Ti.Gesture.orientation == Titanium.UI.LANDSCAPE_LEFT || Ti.Gesture.orientation == Titanium.UI.LANDSCAPE_RIGHT) {
+    $.commencementWindow.backgroundImage = options.mainBackgroundImageLandscape;
+    $.commencementDashboard.height = options.dashboardHeightLandscape;
+    $.commencementDashboard.width = options.dashboardWidthLandscape;
+    $.commencementDashboard.bottom = options.dashboardBottomLandscape;
+    $.commencementDashboard.left = (Alloy.Globals.isLargeScreen()) ? 40 : 10;
+}
+
+Ti.Gesture.addEventListener('orientationchange', function(ev) {
+    if(Alloy.Globals.isLandscape(ev.orientation)) {
+        $.commencementWindow.backgroundImage = options.mainBackgroundImageLandscape;
+        $.commencementDashboard.height = options.dashboardHeightLandscape;
+        $.commencementDashboard.width = options.dashboardWidthLandscape;
+        $.commencementDashboard.bottom = options.dashboardBottomLandscape;
+        $.commencementDashboard.left = (Alloy.Globals.isLargeScreen()) ? 40 : 10;
+    } else {
+        $.commencementWindow.backgroundImage = options.mainBackgroundImage;
+        $.commencementDashboard.height = options.dashboardHeight;
+        $.commencementDashboard.width = options.dashboardWidth;
+        $.commencementDashboard.bottom = options.dashboardBottom;
+        $.commencementDashboard.left = "auto";
+    }
+});
--- app/controllers/index.js
+++ app/controllers/index.js
@@ -50,10 +50,28 @@ if (OS_IOS || OS_MOBILEWEB) {
     }
 }
 
-var imageSuffix = '';
+var imageSuffix = '',
+    screenHeight = Ti.Platform.displayCaps.platformHeight,
+    srceenWidth = Ti.Platform.displayCaps.platformWidth;
 
-if(Alloy.Globals.__isLargeScreen) {
+if(Alloy.Globals.isLargeScreen()) {
+    $.news.height = 170;
+    $.news.width = 204;
+    $.people.height = 170;
+    $.people.width = 204;
+    $.links.height = 170;
+    $.links.width = 204;
+    $.connected.height = 170;
+    $.connected.width = 204;
+    $.services.height = 170;
+    $.services.width = 204;
+    $.emergency.height = 170;
+    $.emergency.width = 204;
     imageSuffix = "_ipad";
+}
+
+if(Alloy.Globals.isAndroid() && Alloy.Globals.isHighDensityScreen()) {
+    imageSuffix = "@2x";
 }
 
 $.news.image = "/images/dashboard/news" + imageSuffix + ".png";
@@ -63,22 +81,92 @@ $.connected.image = "/images/dashboard/connected" + imageSuffix + ".png";
 $.services.image = "/images/dashboard/services" + imageSuffix + ".png";
 $.emergency.image = "/images/dashboard/emergency" + imageSuffix + ".png";
 
+// Set proper dashboard properties
+if(Alloy.Globals.isLargeScreen()) {
+    options = {
+        mainBackgroundImage: '/images/home_ipad.png',
+        dashboardHeight: 340,
+        dashboardWidth: 612,
+        mainBackgroundImageLandscape: '/images/home_ipad_l.png',
+        dashboardHeightLandscape: 510,
+        dashboardWidthLandscape: 410,
+        dashboardBottom: 120,
+        dashboardBottomLandscape: 130
+    };
+} else if(Alloy.Globals.isHighDensityScreen() && Alloy.Globals.isAndroid()) {
+    options = {
+        dashboardHeight: 170,
+        dashboardWidth: 306,
+        dashboardHeightLandscape: 260,
+        dashboardWidthLandscape: 220,
+        mainBackgroundImage: '/images/home@2x.png',
+        mainBackgroundImageLandscape: '/images/home_l@2x.png',
+        dashboardBottom: 40,
+        dashboardBottomLandscape: 20
+    };
+    Ti.API.info(screenHeight);
+    // scale for android heights
+    if(screenHeight == '569.0') {
+        options.mainBackgroundImage = '/images/home_854.png';
+        options.mainBackgroundImageLandscape = '/images/home_854_l.png';
+        options.dashboardBottom = 80;
+    } else if(screenHeight == '533.0') {
+        options.mainBackgroundImage = '/images/home_800.png';
+        options.mainBackgroundImageLandscape = '/images/home_800_l.png';
+    }
+} else {
+    options = {
+        mainBackgroundImage: '/images/home.png',
+        mainBackgroundImageLandscape: '/images/home_l.png',
+        dashboardHeight: 170,
+        dashboardWidth: 306,
+        dashboardBottom: 40,
+        dashboardHeightLandscape: 260,
+        dashboardWidthLandscape: 220,
+        dashboardBottomLandscape: 20
+    };
+}
+
+// Set initial orientation
+if(Ti.Gesture.orientation == Titanium.UI.PORTRAIT || Ti.Gesture.orientation == Titanium.UI.UPSIDE_PORTRAIT) {
+    if (OS_IOS || OS_MOBILEWEB) {
+        $.win.backgroundImage = options.mainBackgroundImage;
+    }
+    $.index.backgroundImage = options.mainBackgroundImage;
+    $.dashboard.height = options.dashboardHeightL
+    $.dashboard.width = options.dashboardWidth;
+    $.dashboard.bottom = options.dashboardBottom;
+    $.dashboard.left = "auto";
+}
 if(Ti.Gesture.orientation == Titanium.UI.LANDSCAPE_LEFT || Ti.Gesture.orientation == Titanium.UI.LANDSCAPE_RIGHT) {
-    $.win.backgroundImage = "/Default-Landscape.png";
+    if (OS_IOS || OS_MOBILEWEB) {
+        $.win.backgroundImage = options.mainBackgroundImageLandscape;
+    }
+    $.index.backgroundImage = options.mainBackgroundImageLandscape;
+    $.dashboard.height = options.dashboardHeightLandscape;
+    $.dashboard.width = options.dashboardWidthLandscape;
+    $.dashboard.bottom = options.dashboardBottomLandscape;
+    $.dashboard.left = (Alloy.Globals.isLargeScreen()) ? 40 : 10;
 }
 
 Ti.Gesture.addEventListener('orientationchange', function(ev) {
     if(Alloy.Globals.isLandscape(ev.orientation)) {
-        $.win.backgroundImage = "/Default-Landscape.png";
-        $.dashboard.height = 260;
-        $.dashboard.width = 220;
-        $.dashboard.bottom = 20;
-        $.dashboard.left = 20;
+        if (OS_IOS || OS_MOBILEWEB) {
+            $.win.backgroundImage = options.mainBackgroundImageLandscape;
+        }
+        $.index.backgroundImage = options.mainBackgroundImageLandscape;
+        $.dashboard.height = options.dashboardHeightLandscape;
+        $.dashboard.width = options.dashboardWidthLandscape;
+        $.dashboard.bottom = options.dashboardBottomLandscape;
+        $.dashboard.left = (Alloy.Globals.isLargeScreen()) ? 40 : 10;
     } else {
-        $.win.backgroundImage = "/Default.png";
-        $.dashboard.height = 170;
-        $.dashboard.width = 306;
-        $.dashboard.bottom = 40;
+        if (OS_IOS || OS_MOBILEWEB) {
+            $.win.backgroundImage = options.mainBackgroundImage;
+        }
+        $.index.backgroundImage = options.mainBackgroundImage;
+        $.dashboard.height = options.dashboardHeight;
+        $.dashboard.width = options.dashboardWidth;
+        $.dashboard.bottom = options.dashboardBottom;
         $.dashboard.left = "auto";
     }
 });
