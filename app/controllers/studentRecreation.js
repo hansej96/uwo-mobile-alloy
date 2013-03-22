8384c65729843d516167e5df67839d1a8e27ac11
Shawn Hansen <shawn.hansen@gmail.com>
2013-03-22 15:28:26 -0500

    Put in Student Rec info and links.

---
+++ app/controllers/studentRecreation.js
@@ -0,0 +1,19 @@
+function newsClick(e) {
+    Ti.Platform.openURL("http://recreation.uwosh.edu/news-box");
+}
+
+function membershipClick(e) {
+    Ti.Platform.openURL("http://recreation.uwosh.edu/studentrecreationfacilities/studentrecreationfacilities/student-recreation-wellness-center-membership");
+}
+
+function featuresClick(e) {
+    Ti.Platform.openURL("http://recreation.uwosh.edu/studentrecreationfacilities/studentrecreationfacilities/srwc-features");
+}
+
+function policiesClick(e) {
+    Ti.Platform.openURL("http://recreation.uwosh.edu/studentrecreationfacilities/studentrecreationfacilities/student-recreation-wellness-center-policies");
+}
+
+function parkingClick(e) {
+    Ti.Platform.openURL("http://recreation.uwosh.edu/studentrecreationfacilities/srwc-parking");
+}
