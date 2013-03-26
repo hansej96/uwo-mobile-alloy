5105423d05082f670c85569fef27548afd45b4c0
Shawn Hansen <shawn.hansen@gmail.com>
2013-03-26 15:20:16 -0500

    Clickified applicable rows in emergency page.

---
+++ app/controllers/emergency.js
@@ -0,0 +1,23 @@
+function titanAlertClick(e) {
+    Ti.Platform.openURL("http://www.uwosh.edu/home/titanalert/");
+}
+
+function campusPoliceClick(e) {
+    Ti.Platform.openURL("tel:9204241212");
+}
+
+function riskManagementClick(e) {
+    Ti.Platform.openURL("tel:9204241191");
+}
+
+function facilitiesClick(e) {
+    Ti.Platform.openURL("tel:9204243466");
+}
+
+function safeWalkClick(e) {
+    Ti.Platform.openURL("tel:9204241212");
+}
+
+function crisisHotlineClick(e) {
+    Ti.Platform.openURL("tel:9202337707");
+}
