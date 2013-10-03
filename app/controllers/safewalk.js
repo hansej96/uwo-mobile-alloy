dd64bcd0d4884021966c9f195b6e2cd1fb48f939
Shawn Hansen <shawn.hansen@gmail.com>
2013-10-03 16:31:41 -0500

    Added initial logic for Safewalk email.

--- app/controllers/safewalk.js
+++ app/controllers/safewalk.js
@@ -1,7 +1,8 @@
-if (OS_IOS || OS_MOBILEWEB) {
-    
-
-} else {
-    
-
-}
+function requestSafeWalkClick(e) {
+        Titanium.Analytics.featureEvent('requestSafewalk');
+        var emailDialog = Ti.UI.createEmailDialog();
+        emailDialog.subject = "Safewalk request";
+        emailDialog.toRecipients = ['hansens@uwosh.edu'];
+        emailDialog.messageBody = '<b>I need a Safewalk!</b>';
+        emailDialog.open();
+    }
