ba1a96ad3b224f580852ed441f91d1794eae0c75
Shawn Hansen <shawn.hansen@gmail.com>
2013-10-04 12:38:40 -0500

    Put splash screens into /app/assets where they belong.

--- app/controllers/safewalk.js
+++ app/controllers/safewalk.js
@@ -1,8 +1,8 @@
 function requestSafeWalkClick(e) {
         Titanium.Analytics.featureEvent('requestSafewalk');
         var emailDialog = Ti.UI.createEmailDialog();
-        emailDialog.subject = "Safewalk request";
+        emailDialog.subject = "Safewalk request from " + $.firstName.value;
         emailDialog.toRecipients = ['hansens@uwosh.edu'];
-        emailDialog.messageBody = '<b>I need a Safewalk!</b>';
+        emailDialog.messageBody = $.firstName.value + " " + $.lastName.value + "\n" + $.emailAddress.value + "\n\n" + $.startingLocation.value + "\n" + $.endingLocation.value + "\n\n" + $.phoneNumber.value + "\n\n" + $.identifier.value;
         emailDialog.open();
     }
