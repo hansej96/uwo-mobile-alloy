c6f68c52b638c5b65d4d3de53f4701b423c16d8e
Shawn Hansen <shawn.hansen@gmail.com>
2013-10-04 15:55:33 -0500

    More safewalk work.

--- app/controllers/safewalk.js
+++ app/controllers/safewalk.js
@@ -1,8 +1,40 @@
 function requestSafeWalkClick(e) {
         Titanium.Analytics.featureEvent('requestSafewalk');
         var emailDialog = Ti.UI.createEmailDialog();
-        emailDialog.subject = "Safewalk request from " + $.firstName.value;
+        emailDialog.subject = "Safewalk request from " + $.firstName.value + " " + $.lastName.value;
         emailDialog.toRecipients = ['hansens@uwosh.edu'];
-        emailDialog.messageBody = $.firstName.value + " " + $.lastName.value + "\n" + $.emailAddress.value + "\n\n" + $.startingLocation.value + "\n" + $.endingLocation.value + "\n\n" + $.phoneNumber.value + "\n\n" + $.identifier.value;
+        emailDialog.messageBody = "FN: " + $.firstName.value + "\n"
+            + "LN: " + $.lastName.value + "\n\n"
+            + "EMAIL: " + $.emailAddress.value + "\n\n"
+            + "SL: " + $.startingLocation.value + "\n\n"
+            + "EL: " + $.endingLocation.value + "\n\n"
+            + "P: " + $.phoneNumber.value + "\n\n"
+            + "ID: " + $.identifier.value;
         emailDialog.open();
+}
+
+if (OS_IOS) {
+    var startingHint = "Please list the address or intersection you are currently located at.";
+    var endingHint = "What address or intersection would you like a CSO to provide a Safewalk to?";
+    var callbackHint = "Please provide a phone number we can reach you at to confirm when our CSOs will arrive at your location.";
+    var identifyHint = "To help us identify you upon arrival, please provide a brief description of your clothing.";
+    
+    startingHintText(startingHint);
+    
+    $.startingLocation.addEventListener('focus', function(e) {
+        if($.startingLocation.value == startingHint){
+            $.startingLocation.setValue("");
+        }
+    });
+    $.startingLocation.addEventListener('blur', function(e) {
+        startingHintText();
+    });
+    
+    function startingHintText(hintText){   
+        if ($.startingLocation.value.length == 0) {
+            $.startingLocation.setValue(hintText);
+        }
+ 
     }
+
+}
