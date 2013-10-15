ef533bee4edde1ea2d8b32282d275c61a7204477
Shawn Hansen <shawn.hansen@gmail.com>
2013-10-15 10:09:05 -0500

    Update Safewalk icons. Logic for successful completion of Safewalk request.

--- app/controllers/safewalk.js
+++ app/controllers/safewalk.js
@@ -2,7 +2,7 @@ function requestSafeWalkClick(e) {
         Titanium.Analytics.featureEvent('requestSafewalk');
         var emailDialog = Ti.UI.createEmailDialog();
         emailDialog.subject = "Safewalk request from " + $.firstName.value + " " + $.lastName.value;
-        emailDialog.toRecipients = ['hansens@uwosh.edu'];
+        emailDialog.toRecipients = ['safewalk@uwosh.edu'];
         emailDialog.messageBody = "FN: " + $.firstName.value + "\n"
             + "LN: " + $.lastName.value + "\n\n"
             + "EMAIL: " + $.emailAddress.value + "\n\n"
@@ -10,31 +10,23 @@ function requestSafeWalkClick(e) {
             + "EL: " + $.endingLocation.value + "\n\n"
             + "P: " + $.phoneNumber.value + "\n\n"
             + "ID: " + $.identifier.value;
-        emailDialog.open();
+        if ($.firstName.value !="" && $.lastName.value !="" && $.emailAddress.value !="" && $.startingLocation.value !="" && $.endingLocation.value !="" && $.phoneNumber.value !="" && $.identifier.value !="") {
+            emailDialog.open();
+        }
+        
+        emailDialog.addEventListener("complete", function(e) {
+            if (OS_IOS || OS_MOBILEWEB) {
+                if (e.result == emailDialog.SENT) {
+                    Titanium.Analytics.featureEvent('safewalkRequestSent');
+                    Alloy.Globals.navwindow.openWindow(Alloy.createController('safewalkCompleted').getView());
+                }
+            } else {
+                if (e.result == emailDialog.SENT) {
+                    Titanium.Analytics.featureEvent('safewalkRequestSent');
+                    Alloy.createController('safewalkCompleted').getView().open();
+                }
+            }
+        });
 }
 
-if (OS_IOS) {
-    var startingHint = "Please list the address or intersection you are currently located at.";
-    var endingHint = "What address or intersection would you like a CSO to provide a Safewalk to?";
-    var callbackHint = "Please provide a phone number we can reach you at to confirm when our CSOs will arrive at your location.";
-    var identifyHint = "To help us identify you upon arrival, please provide a brief description of your clothing.";
-    
-    startingHintText(startingHint);
-    
-    $.startingLocation.addEventListener('focus', function(e) {
-        if($.startingLocation.value == startingHint){
-            $.startingLocation.setValue("");
-        }
-    });
-    $.startingLocation.addEventListener('blur', function(e) {
-        startingHintText();
-    });
-    
-    function startingHintText(hintText){   
-        if ($.startingLocation.value.length == 0) {
-            $.startingLocation.setValue(hintText);
-        }
- 
-    }
 
-}
