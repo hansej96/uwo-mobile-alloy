f5ad19c9a6c8ac5f104c5ee01321de61806a1155
Jon Hansen <hansej96@uwosh.edu>
2014-04-18 13:51:42 -0500

    added calling permissions and android call funtion

--- app/controllers/emergency.js
+++ app/controllers/emergency.js
@@ -5,28 +5,77 @@ function titanAlertClick(e) {
     	title: "Titan Alert"
     }).getView());
 }
+if (OS_IOS || OS_MOBILEWEB) {
+	
+	function campusPoliceClick(e) {
+    	Titanium.Analytics.featureEvent('callCampusPolice');
+ 	   Ti.Platform.openURL("telprompt:9204241212");
+	}
 
-function campusPoliceClick(e) {
-    Titanium.Analytics.featureEvent('callCampusPolice');
-    Ti.Platform.openURL("telprompt:9204241212");
-}
+	function riskManagementClick(e) {
+ 	   Titanium.Analytics.featureEvent('callRiskManagement');
+ 	   Ti.Platform.openURL("telprompt:9204241191");
+	}
 
-function riskManagementClick(e) {
-    Titanium.Analytics.featureEvent('callRiskManagement');
-    Ti.Platform.openURL("telprompt:9204241191");
-}
+	function facilitiesClick(e) {
+	    Titanium.Analytics.featureEvent('callFacilities');
+	    Ti.Platform.openURL("telprompt:9204243466");
+	}
 
-function facilitiesClick(e) {
-    Titanium.Analytics.featureEvent('callFacilities');
-    Ti.Platform.openURL("telprompt:9204243466");
-}
+	function safeWalkClick(e) {
+	    Titanium.Analytics.featureEvent('callSafeWalk');
+	    Ti.Platform.openURL("telprompt:9204241212");
+	}
 
-function safeWalkClick(e) {
-    Titanium.Analytics.featureEvent('callSafeWalk');
-    Ti.Platform.openURL("telprompt:9204241212");
-}
+	function crisisHotlineClick(e) {
+   	 Titanium.Analytics.featureEvent('callCrisisHotline');
+  	  Ti.Platform.openURL("telprompt:9202337707");
+	}
+	
+} else {
+	
+	function campusPoliceAndroidClick(e) {
+       Titanium.Analytics.featureEvent('callCampusPolice');
+ 	   var intent = Ti.Android.createIntent({
+			action: Ti.Android.ACTION_DIAL,
+			data: 'tel:9204241212'
+		});
+		Ti.Android.currentActivity.startActivity(intent);
+	}
 
-function crisisHotlineClick(e) {
-    Titanium.Analytics.featureEvent('callCrisisHotline');
-    Ti.Platform.openURL("telprompt:9202337707");
+	function riskManagementAndroidClick(e) {
+ 	   Titanium.Analytics.featureEvent('callRiskManagement');
+		var intent = Ti.Android.createIntent({
+			action: Ti.Android.ACTION_DIAL,
+			data: 'tel:9204241191'
+		});
+		Ti.Android.currentActivity.startActivity(intent);
+	}
+
+	function facilitiesAndroidClick(e) {
+	    Titanium.Analytics.featureEvent('callFacilities');
+	    var intent = Ti.Android.createIntent({
+			action: Ti.Android.ACTION_DIAL,
+			data: 'tel:9204243466'
+		});
+		Ti.Android.currentActivity.startActivity(intent);
+	}
+
+	function safeWalkAndroidClick(e) {
+	    Titanium.Analytics.featureEvent('callSafeWalk');;
+	    var intent = Ti.Android.createIntent({
+			action: Ti.Android.ACTION_DIAL,
+			data: 'tel:9204241212'
+		});
+		Ti.Android.currentActivity.startActivity(intent);
+	}
+
+	function crisisHotlineAndroidClick(e) {
+   	 Titanium.Analytics.featureEvent('callCrisisHotline');
+  	  var intent = Ti.Android.createIntent({
+			action: Ti.Android.ACTION_DIAL,
+			data: 'tel:9202337707'
+		});
+		Ti.Android.currentActivity.startActivity(intent);
+	}
 }
